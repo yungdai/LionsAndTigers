b08ce91c189ad19211fc035b795539997da78ba9
Yung Dai <yungchidai@gmail.com>
2015-02-03 16:15:44 -0500

    Lions and Tigers!
    Initial lesson to build a struct in iOS 8 and swift

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Yung Dai on 2015-02-03.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+}
